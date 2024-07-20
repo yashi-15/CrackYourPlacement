edf53a608574eb5e5dd1b4f9635eec3f7304d2de
yashi-15 <sharmayashi270@gmail.com>
2024-07-20 23:33:54 +0530

    day 1 and 3

---
+++ String/Day-1,2,3.py
@@ -0,0 +1,24 @@
+# D-1
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+
+# D-3 
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
